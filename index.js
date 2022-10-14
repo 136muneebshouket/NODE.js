c11a4d50bab2c5717f24e9c95c3095004576cd56
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2022-10-14 17:11:25 +0500

    creating a static html file on node js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 
 
 const server = http.createServer((req,res)=>{
     res.statusCode=200;
-    res.setHeader('Content-Type','text/html')
-    res.end('<h1>hello i am running</h1>');
+       const data = fs.readFileSync('index.html')
+
+    res.end(data.toString());
 
 
 })
