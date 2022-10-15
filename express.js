6618acb7b74587fbca68fddfccdde0d0280e558a
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2022-10-15 13:53:17 +0500

    working perfectly

--- express.js
+++ express.js
@@ -3,13 +3,13 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log(path.join(__dirname,'../website'));
-const staticpath =path.join(__dirname,'../website')
+console.log(path.join(__dirname+'/website/index.html'));
+const staticpath =path.join(__dirname+'/website/index.html')
 
 app.use(express.static(staticpath));
 
 app.get("/",(req,res)=>{
- res.send(staticpath);
+ res.sendFile(staticpath);
 })
 
 
